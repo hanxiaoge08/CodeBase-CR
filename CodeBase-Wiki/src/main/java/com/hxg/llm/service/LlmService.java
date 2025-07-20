f0d8e8bd2e5eec8203dc50cf489623414892ec60
hanxiaoge <1072407118@qq.com>
2025-07-20 13:08:19 +0800

    refactor(review): rename module

---
+++ CodeBase-Wiki/src/main/java/com/hxg/llm/service/LlmService.java
@@ -0,0 +1,21 @@
+package com.hxg.llm.service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author hxg
+ * @description: TODO
+ * @date 2025/7/20 00:42
+ */
+//@Service
+//public class LlmService {
+//    private ChatClient chatClient;
+//    private MessageWindowChatMemory chatMemory;
+//    private final int MAX_HISTORY_MESSAGES=20;
+//    private ToolCallback[] allTools;
+//
+//    public LlmService(ChatClient.Builder chatClientBuilder,SQLite)
+//}
